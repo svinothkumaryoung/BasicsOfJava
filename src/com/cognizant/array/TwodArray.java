126b8b2fecfa83de3993305e8c5b821b90a5cd90
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-04-01 16:49:11 +0530

    Constructor

--- src/com/cognizant/array/TwodArray.java
+++ src/com/cognizant/array/TwodArray.java
@@ -2,30 +2,31 @@ package com.cognizant.array;
 
 import java.util.Scanner;
 
-public class TwodArray {
+public class TwodArray
+{
     String roomNo[][]=new String[3][3];
     Scanner s1=new Scanner(System.in);
     void getData()
     {
-    for(int i=0;i<roomNo.length;i++)
-    {
-    for(int j=0;j<roomNo[i].length;j++)
-    {
-        System.out.println("Enter the Product U want to store it on room no "+(i+1)+(j+1));
-        roomNo[i][j]=s1.next();
-    }
-    }
+        for(int i=0;i<roomNo.length;i++)
+        {
+            for(int j=0;j<roomNo[i].length;j++)
+            {
+                System.out.println("Enter the Product U want to store it on room no "+(i+1)+(j+1));
+                roomNo[i][j]=s1.next();
+            }
+        }
     }
     void printData()
     {
-    System.out.println("****Details present on Room No is ****");
-    for(int i=0;i<roomNo.length;i++)
-    {
-    for(int j=0;j<roomNo[i].length;j++)
-    {
-    System.out.println("Product stored on room no "+(i+1)+(j+1)+" "+roomNo[i][j]);
-    }
-    }
+        System.out.println("****Details present on Room No is ****");
+        for(int i=0;i<roomNo.length;i++)
+            {
+                for(int j=0;j<roomNo[i].length;j++)
+                {
+                System.out.println("Product stored on room no "+(i+1)+(j+1)+" "+roomNo[i][j]);
+                }
+            }
     }
 
     public static void main(String[] args) {
